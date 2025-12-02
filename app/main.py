d3d2b5b5fcf6f1b92f6baca08d282cf5683d9bf6
Aleksey Znamenskiy <aznmsky@gmail.com>
2025-12-02 07:23:24 +0300

    #4 обновлен req.txt

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
 from app.routers.main import main_router
 
 
 app = FastAPI(
     title='Pyntropy',
-    description='Курсы по ИТ для системных аналитиков.',
+    description='Обучение системных аналитиков.',
     version='0.0.1')
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(main_router)
-
-
 
